61ee6120de208dd20248fee9791eac542cf9e8e3
Jobsidney <jobsidney67@gmail.com>
2022-08-17 18:24:41 +0300

    commit tree 8c60ebc23e18ed5437c5c3a09d8c88f614f64dc9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map{
+        |name|
+        badge_maker(name)
+    }
+end    
+
+def assign_rooms(arr)
+    arr.map.with_index{
+        |item,index|
+        if index<7
+            "Hello, #{item}! You'll be assigned to room #{index+1}!"
+        end
+}
+end
+
+def printer(array)
+    badge=batch_badge_creator(array)
+    rooms=assign_rooms(array)
+    def outp(data)
+        data.each{
+            |item|
+            puts item
+        }
+    end
+    outp(badge)
+    outp(rooms)
+end
+printer(["shee","kelly"])
